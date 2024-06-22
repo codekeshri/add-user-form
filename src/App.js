724a19d0c316dd51cd910bdcd1dfa67d88894162
codekeshri <arvidce@gmail.com>
2024-06-22 20:21:57 +0530

    A Simple add-user-form in React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import AddUser from './components/AddUserForm/AddUserForm';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Arvind Keshri</h1>
+      <AddUser />
+    </div>
+  );
+}
+
+export default App;
